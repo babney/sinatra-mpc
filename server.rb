569c232963b3d08454cd3796565b56b32feb5bbd
Bill Abney <bill.abney@gmail.com>
2010-12-11 16:02:42 -0800

    get rid of the global mpc handle, it times out

--- server.rb
+++ server.rb
@@ -8,33 +8,37 @@ require 'sinatra'
 require 'settings'
 
 #ugh, this seems really dangerous but I don't feel like writing a wrapper around Mpc at the moment
-$mpc = Mpc.new(MPD_HOST, MPD_PORT)
-$library = $mpc.list_library
+mpc = Mpc.new(MPD_HOST, MPD_PORT)
+$library = mpc.list_library
 
 get '/' do
   erb :"index.html"
 end
 
 get '/next' do
-  $mpc.next()
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
+  @mpc.next()
   send_current_track
 end
 
 get '/prev' do
-  $mpc.previous()
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
+  @mpc.previous()
   send_current_track  
 end
 
 get '/stop' do
-  $mpc.stop()
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
+  @mpc.stop()
   send_current_track
 end
 
 get '/playpause' do
-  if !$mpc.playing?
-    $mpc.play
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
+  if !@mpc.playing?
+    @mpc.play
   else
-    $mpc.pause
+    @mpc.pause
   end
   send_current_track
 end
@@ -45,24 +49,25 @@ end
 
 get '/current_playlist' do
   content_type :json
-  #this explodes when using the global mpc connection, need to find out why
   @mpc = Mpc.new(MPD_HOST, MPD_PORT)
   @mpc.current_playlist_songs.to_json
 end
 
 get '/switch_track' do
-  $mpc.seek(0,params[:pos])
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
+  @mpc.seek(0,params[:pos])
   send_current_track
 end
 
 def send_current_track
+  @mpc = Mpc.new(MPD_HOST, MPD_PORT)
   content_type :json
   sym = ""
-  if $mpc.playing?
+  if @mpc.playing?
     sym = "||"
   else
     sym = ">"
   end
-  current = $mpc.current_song
+  current = @mpc.current_song
   {:title => current[:title], :album => current[:album], :artist => current[:artist], :sym => sym}.to_json
 end
